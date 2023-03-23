e19c963a94002e99ccb3f125bdac53623212e859
Abdelrahman <abdelrahman19.adel@gmail.com>
2023-03-23 14:58:55 +0200

    FibonacciNumber

---
+++ week2/FibNum.py
@@ -0,0 +1,13 @@
+def FibonacciNumber(num):
+    pass
+
+
+num = int(input("please input a number "))
+arr = []
+arr.append(0)
+arr.append(1)
+
+for i in range(2, num+1):
+    arr.append(arr[i-1]+arr[i-2])
+
+print(arr[-1])
