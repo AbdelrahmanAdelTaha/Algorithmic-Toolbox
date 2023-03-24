9f0dd58d968172009ddf21bbd8b62a6f74749104
Abdelrahman <abdelrahman19.adel@gmail.com>
2023-03-24 10:37:30 +0200

    GCD

---
+++ week2/GCD.py
@@ -0,0 +1,30 @@
+num1 = int(input("Please Enter First Number "))
+num2 = int(input("Please Enter Second Number "))
+
+if (num1 % 2 != 0) and (num2 % 2 != 0):
+    GCD = 1
+
+if num2 > num1:
+    temp = num2
+    num2 = num1
+    num1 = temp
+
+
+# A list for the first number - B list for the second number - Q list for the Quotient - R list for the Reminder
+A = []
+B = []
+Q = []
+R = []
+
+A.append(num1)
+B.append(num2)
+
+while B[-1] != 0:
+    Q.append(int(A[-1] / B[-1]))
+    R.append(A[-1] - (B[-1]*Q[-1]))
+    A.append(B[-1])
+    B.append(R[-1])
+
+GCD = A[-1]
+
+print(GCD)
